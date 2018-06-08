8c1f3872ecf09cead54dd38b6c3d30d9765e4327
doug <drcox23@gmail.com>
2018-06-07 18:17:43 -1000

    commit

--- acceptable-sequence/acceptableSequence.js
+++ acceptable-sequence/acceptableSequence.js
@@ -1,3 +1,17 @@
 module.exports = function acceptableSequence(str) {
   // write your code here
+// regular expressions --
+
+  for (let i = 0; i< str.length; i++) {
+    if(str[0].match(/[a-z]/i) || str[str.length - 1].match(/[a-z]/i)){
+      return false;
+    }else{
+      if (str[i].match(/[a-z]/i) && (str[i-1] !== "+" || str[i + 1] !== "+")){
+        return false;
+      }
+    }
+   }
+return true
+
 }
+
--- check-ascending-order/checkAscOrder.js
+++ check-ascending-order/checkAscOrder.js
@@ -1,3 +1,13 @@
 module.exports = function checkAscOrder(numArray) {
   // write your code in here
+  // check ascending order
+
+for (var i=0; i<numArray.length-1; i++) {
+
+  if (numArray[i] > numArray[i+1]){
+    return false;
+  }
+}
+return true;
+
 }
